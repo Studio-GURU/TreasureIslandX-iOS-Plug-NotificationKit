78879914ef14d225b15147294ff42bc2274afed7
SunWoo.Kim <sunwoo@gurucompany.co.kr>
2025-01-17 18:17:50 +0900

    TreasureIslandPlugNotificationKit 25.1.9 Release

---
+++ TreasureIslandPlugNotificationKit.xcframework/ios-arm64/TreasureIslandPlugNotificationKit.framework/Headers/TreasureIslandPlugNotificationKit.h
@@ -0,0 +1,18 @@
+//
+//  TreasureIslandPlugNotificationKit.h
+//  TreasureIslandPlugNotificationKit
+//
+//  Created by sunwoo on 7/15/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TreasureIslandPlugNotificationKit.
+FOUNDATION_EXPORT double TreasureIslandPlugNotificationKitVersionNumber;
+
+//! Project version string for TreasureIslandPlugNotificationKit.
+FOUNDATION_EXPORT const unsigned char TreasureIslandPlugNotificationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TreasureIslandPlugNotificationKit/PublicHeader.h>
+
+
